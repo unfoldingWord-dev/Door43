3552355249cff7275dd75089e9c15c30855b85d3
Richard Mahn <richmahn@gmail.com>
2015-06-25 16:18:40 -0600

    updated bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,52 @@
 <?php
+// Turn off output buffering
+ini_set('output_buffering', 'off');
+// Turn off PHP output compression
+ini_set('zlib.output_compression', false);
+         
+//Flush (send) the output buffer and turn off output buffering
+//ob_end_flush();
+while (@ob_end_flush());
 
-`cp conf/local.php.dev conf/local.php`;
-`cp conf/plugin.local.php.dev conf/plugin.local.php`;
-`cp conf/acl.auth.php.dev conf/acl.auth.php`;
-`cp conf/users.auth.php.dev conf/users.auth.php`;
+// Implicitly flush the buffer(s)
+ini_set('implicit_flush', true);
+ob_implicit_flush(true);
 
-`git config core.fileMode false`;
+//prevent apache from buffering it for deflate/gzip
+header( 'Content-type: text/html; charset=utf-8' );
+header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+  
+for($i = 0; $i < 1000; $i++)
+{
+  echo ' ';
+}
+           
+flush();
 
-`git submodule init`;
-`git submodule update`;
+/// Now start the program output
+?>
+<head>
+	<title>Bootstrap</title>
+</head>
+<body>
+<?php
+echo 'Putting config files in place...<br/>';
+echo `cp conf/local.php.dev conf/local.php`;
+echo `cp conf/plugins.local.php.dev conf/plugin.local.php`;
+echo `cp conf/acl.auth.php.dev conf/acl.auth.php`;
+echo `cp conf/users.auth.php.dev conf/users.auth.php`;
 
-echo 'OK! Proceed to <a href="home">Home page</a>';
+flush();
+echo 'Making git configurations...<br/>';
+echo `git config core.fileMode false`;
+
+flush();
+echo 'Pulling submodules...<br/>';
+echo `git submodule init`;
+echo `git submodule update`;
+?>
+<p>
+OK! Proceed to <a href="home">Home page</a>
+</p>
+</body>
+
