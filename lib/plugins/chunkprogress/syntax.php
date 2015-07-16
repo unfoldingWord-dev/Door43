55ca37c530dabc1bd95ddbcf86c894e35e69fd24
Craig Oliver <craig_oliver@wycliffeassociates.org>
2015-07-16 15:25:59 -0400

    Accept and validate parameters

--- lib/plugins/chunkprogress/syntax.php
+++ lib/plugins/chunkprogress/syntax.php
@@ -32,18 +32,44 @@ class syntax_plugin_chunkprogress extends DokuWiki_Syntax_Plugin {
 
     function handle($match, $state, $pos, &$handler){
         $params = array(
-            "page" => "(no page)",
-            "raw_match" => $match
+            "page" => "",
         );
 
-        
 
-
+        // Extract parameter string from match
+        $begin_pos = strpos($match, ">") + 1;
+        $end_pos = strpos($match, "}") - 1;
+        $all_params_string = substr($match, $begin_pos, $end_pos - $begin_pos + 1);
+        // Process parameters
+        try {
+            foreach(explode("&", $all_params_string) as $param_string) {
+                $param_pair = explode("=", $param_string);
+                if (count($param_pair) == 2) {
+                    $key = $param_pair[0];
+                    $value = $param_pair[1];
+                    if (array_key_exists($key, $params)) {
+                        $params[$key] = $value;
+                    } else {
+                        $params["message"] .= "\nWARNING: didn't recognize parameter '" . $key . "' (maybe you misspelled it?)";
+                    }
+                } else {
+                    $params["message"] .= "\nWARNING: didn't understand parameter '" . $param_string . "' (maybe you forgot the '=''?)";
+                }
+            }
+        } catch (Exception $exception) {
+            $params["message"] .= "EXCEPTION: Please tell a developer about this: " . $e->getMessage();
+        }
 
         return $params;
     }
 
     function render($mode, &$renderer, $params) {
+        // Print warnings or errors, if any
+        if (array_key_exists("message", $params)) {
+            $renderer->strong_open();
+            $renderer->unformatted($params["message"]);
+            $renderer->strong_close();
+        }
         $renderer->table_open();
         $renderer->tablerow_open();
         $renderer->tablecell_open();
