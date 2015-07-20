1c1b237fdc37dcd2438bde4dba21826210372f26
Craig Oliver <craig_oliver@wycliffeassociates.org>
2015-07-20 10:28:17 -0400

    Updated docs to match PEAR standard

--- lib/plugins/chunkprogress/syntax.php
+++ lib/plugins/chunkprogress/syntax.php
@@ -23,36 +23,76 @@ if (!defined('DOKU_PLUGIN')) {
 require_once DOKU_PLUGIN.'syntax.php';
 
 
+/**
+ * Creates a report showing the progress of a chunk through its stages.
+ *
+ * @category Door43
+ * @package  DokuWiki
+ * @author   Craig Oliver <craig_oliver@wycliffeassociates.org>
+ * @license  GPL2 (I think)
+ * @link     ???
+ */
 class syntax_plugin_chunkprogress extends DokuWiki_Syntax_Plugin
 {
 
+    /** 
+     * Gets the info block for this plugin
+     * @return the info block for this plugin 
+     */
     public function getInfo()
     {
         return confToHash(dirname(__FILE__).'/plugin.info.txt');
     }
 
+    /** 
+     * Gets the type for this plugin
+     * @return "substition"
+     */
     public function getType()
     {
         return "substition";
     }
 
+    /** 
+     * Gets the paragraph type of this plugin
+     * @return "block"
+     */
     public function getPType()
     {
         return "block";
     }
 
+    /** 
+     * Gets the sort order for this plugin
+     * @return the sort order (1 for now, 999 doesn't work)
+     */
     public function getSort()
     {
         return 1;
     }
 
+    /** 
+     * Connects the plugin to the text that triggers it
+     * @param string $mode (TODO: not sure what this is for)
+     * @return None
+     */
     public function connectTo($mode)
     {
-        $this->Lexer->addSpecialPattern('\{\{chunkprogress>[^}]*\}\}', $mode, 'plugin_chunkprogress');
+        $this->Lexer->addSpecialPattern(
+            '\{\{chunkprogress>[^}]*\}\}', $mode, 'plugin_chunkprogress'
+        );
     }
 
 
-    public function handle($match, $state, $pos, &$handlerl)
+    /** 
+     * Pulls the data necessary for rendering
+     * @param string $match   the text matched by the pattern
+     * @param string $state   The type of pattern
+     * @param int    $pos     character position of matched text
+     * @param obj    $handler ref to the Doku_Handler object
+     * @return the parameters for render()
+     */
+    public function handle($match, $state, $pos, &$handler)
     {
         $params = array(
             "page" => "",
@@ -74,16 +114,19 @@ class syntax_plugin_chunkprogress extends DokuWiki_Syntax_Plugin
                     if (array_key_exists($key, $params)) {
                         $params[$key] = $value;
                     } else {
-                        $params["message"] .= "\nWARNING: didn't recognize parameter '" .
+                        $params["message"] .= 
+                            "\nWARNING: didn't recognize parameter '" .
                             $key . "' (maybe you misspelled it?)";
                     }
                 } else {
-                    $params["message"] .= "\nWARNING: didn't understand parameter '" .
+                    $params["message"] .= 
+                        "\nWARNING: didn't understand parameter '" .
                         $param_string . "' (maybe you forgot the '=''?)";
                 }
             }
         } catch (Exception $exception) {
-            $params["message"] .= "EXCEPTION: Please tell a developer about this: " . $e->getMessage();
+            $params["message"] .= 
+                "EXCEPTION: Please tell a developer about this: " . $e->getMessage();
         }
 
 
@@ -94,7 +137,8 @@ class syntax_plugin_chunkprogress extends DokuWiki_Syntax_Plugin
                 $params["page_title"] = $metadata["title"];
             } else {
                 $params["message"] .= "WARNING: Could not find a page named '" .
-                    $params["page"] . "'.  Did you use the form 'en:bible:notes:1ch:01:01'?";
+                    $params["page"] . 
+                    "'.  Did you use the form 'en:bible:notes:1ch:01:01'?";
             }
         }
 
@@ -103,6 +147,13 @@ class syntax_plugin_chunkprogress extends DokuWiki_Syntax_Plugin
         return $params;
     }
 
+    /** 
+     * Renders the data to the page
+     * @param string $mode     Name of the format mode 
+     * @param obj    $renderer ref to the Doku_Renderer
+     * @param obj    $params   Parameter object returned by handle()
+     * @return the parameters for render()
+     */
     public function render($mode, &$renderer, $params)
     {
 
