33e880abb6069e657679816f537f5f0acaf7e625
Craig Oliver <craig_oliver@wycliffeassociates.org>
2015-08-12 15:18:20 -0400

    Added an initial unit test.
    It's a copy of the general.test.php from door43obs.  Confirmed to execute and
    pass without errors.

---
+++ lib/plugins/chunkprogress/_test/general.test.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * PHP version 5
+ *
+ * Activity by user report
+ *
+ * @category Door43
+ * @package  Chunkprogress
+ * @author   Craig Oliver <craig_oliver@wycliffeassociates.org>
+ * @license  GPL2 (I think)
+ * @link     ???
+ */
+
+/**
+ * General tests
+ *
+ * @category Door43
+ * @package  Chunkprogress
+ * @author   Craig Oliver <craig_oliver@wycliffeassociates.org>
+ * @license  GPL2 (I think)
+ * @link     ???
+ */
+class general_plugin_door43obs_test extends DokuWikiTest
+{
+
+    /**
+     * Simple test to make sure the plugin.info.txt is in the correct format
+     * @return nothing
+     */
+    public function test_plugininfo()
+    {
+        $file = __DIR__.'/../plugin.info.txt';
+        $this->assertFileExists($file);
+
+        $info = confToHash($file);
+
+        $this->assertArrayHasKey('base', $info);
+        $this->assertArrayHasKey('author', $info);
+        $this->assertArrayHasKey('email', $info);
+        $this->assertArrayHasKey('date', $info);
+        $this->assertArrayHasKey('name', $info);
+        $this->assertArrayHasKey('desc', $info);
+        $this->assertArrayHasKey('url', $info);
+
+        $this->assertEquals('chunkprogress', $info['base']);
+        $this->assertRegExp('/^https?:\/\//', $info['url']);
+        $this->assertTrue(mail_isvalid($info['email']));
+        $this->assertRegExp('/^\d\d\d\d-\d\d-\d\d$/', $info['date']);
+        $this->assertTrue(false !== strtotime($info['date']));
+    }
+}
