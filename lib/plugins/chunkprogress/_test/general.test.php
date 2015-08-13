496fa321e70def3285b76b2bc1f97e9341cf436f
Craig Oliver <craig_oliver@wycliffeassociates.org>
2015-08-13 16:51:09 -0400

    Add unit test for activity-by-user report

--- lib/plugins/chunkprogress/_test/general.test.php
+++ lib/plugins/chunkprogress/_test/general.test.php
@@ -23,27 +23,6 @@
 class general_plugin_chunkprogress_test extends DokuWikiTest
 {
 
-    /**
-     * Set up before class
-     * @return nothing
-     */
-    public static function setUpBeforeClass() 
-    {
-        parent::setUpBeforeClass();
-
-        // copy the test data
-        TestUtils::rcopy(TMP_DIR, dirname(__FILE__) . '/data/');
-    }
-
-    /**
-     * Set up before test
-     * @return nothing
-     */
-    public function setUp() 
-    {
-        $this->pluginsEnabled[] = 'chunkprogress';
-        parent::setUp();
-    }
 
     /**
      * Simple test to make sure the plugin.info.txt is in the correct format
@@ -71,43 +50,4 @@ class general_plugin_chunkprogress_test extends DokuWikiTest
         $this->assertTrue(false !== strtotime($info['date']));
     }
 
-    /**
-     * Run activity by namespace report, verify correct values appear 
-     * @return nothing
-     */
-    public function test_activity_by_namespace()
-    {
-        $thisPlugin = plugin_load('syntax', 'chunkprogress');
-        $request = new TestRequest();
-        $response = $request->get(
-            array('id' => 'test_activity_by_namespace'), '/doku.php'
-        );
-        $content = $response->getContent();
-
-        $this->assertNotEmpty($content);
-
-        // Verify totals
-        $this->assertRegExp(
-            "/TOTAL<[^>]*><[^>]*>6<[^>]*><[^>]*>35<[^>]*><[^>]*>30/", $content
-        );
-
-        // Verify timestamps were understood correctly
-        $this->assertRegExp(
-            "/start_timestamp<[^>]*><[^>]*>1435723200/", $content
-        );
-        $this->assertRegExp(
-            "/end_timestamp<[^>]*><[^>]*>1435982400/", $content
-        );
-
-        // Verify page counts 
-        $this->assertRegExp(
-            "/debug_num_pages_in_ns<[^>]*><[^>]*>70/", $content
-        );
-        $this->assertRegExp(
-            "/debug_num_revisions_in_ns<[^>]*><[^>]*>2039/", $content
-        );
-        $this->assertRegExp(
-            "/debug_num_revisions_within_dates<[^>]*><[^>]*>169/", $content
-        );
-    }
 }
