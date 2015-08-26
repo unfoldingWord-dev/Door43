af39e29d54a7cbe94a8683f46a2bdadbe908c662
K Builder <k@kbuildsyour.com>
2015-08-26 01:55:05 -0700

    Added Apply All & Dismiss All options for admins.

--- lib/plugins/door43gitmerge/lang/en/lang.php
+++ lib/plugins/door43gitmerge/lang/en/lang.php
@@ -14,11 +14,16 @@
 $lang['back'] = 'Show page';
 $lang['merge'] = 'Apply Updates';
 $lang['version_to_compare'] = 'Version To Compare';
+$lang['switch_to'] = 'Switch To';
 $lang['show_all'] = 'Show All';
+$lang['show'] = 'Show';
+$lang['all'] = 'All';
 $lang['dismiss'] = 'Dismiss';
+$lang['dismiss_all'] = 'Dismiss All';
 $lang['edit_and_apply'] = 'Edit &amp; Apply';
 $lang['cancel'] = 'Cancel';
 $lang['reset'] = 'Reset';
 $lang['apply'] = 'Apply';
+$lang['apply_all'] = 'Apply All';
 
 //Setup VIM: ex: et ts=4 :
