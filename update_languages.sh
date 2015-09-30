30370d1180d51b6a058401b268c122014b779ae9
Richard Mahn <richard_mahn@wycliffeassociates.org>
2015-09-30 19:24:36 +0000

    update to the update_languages.sh script to accept a single language

--- update_languages.sh
+++ update_languages.sh
@@ -18,31 +18,58 @@ $DEBUG && set -x
 
 : ${DOOR43_DIR:=$(cd $(dirname "$0") > /dev/null && pwd)}
 
-# Let the person running the script know what's going on.
-echo "Pulling in latest changes for all language repositories..."
+if [ -z $1 ];
+then
+        # default installed languages
+        LANGUAGES="ALL"
+else
+        LANGUAGES=($@)
+fi
 
-# going to the parent directory
-pushd "$DOOR43_DIR/data/gitrepo/pages" > /dev/null
+if [ -z $1 ];
+then
+  # Let the person running the script know what's going on.
+  echo "Pulling in latest changes for all language repositories..."
 
-# Find all git repositories and update it to the master latest revision
-for dir in $(find . -maxdepth 2 -type d -name ".git" | cut -c 3-); do
-    echo "";
-    echo "Updateing $(dirname "$dir")";
+  # going to the parent directory
+  pushd "$DOOR43_DIR/data/gitrepo/pages" > /dev/null
 
-    # We have to go to the .git parent directory to call the pull command
-    pushd "$dir/.." > /dev/null;
+  # Find all git repositories and update it to the master latest revision
+  for dir in $(find . -maxdepth 2 -type d -name ".git" | cut -c 3- | sort -u); do
+      echo "";
+      echo "Updateing $(dirname "$dir")";
 
-    # we don't want any changes in a development language branch
-    git reset --hard
+      # We have to go to the .git parent directory to call the pull command
+      pushd "$dir/.." > /dev/null;
 
-    # finally pull
-    git pull origin master;
+      # we don't want any changes in a development language branch
+      git fetch --all
+      git reset --hard origin/master
 
-    # lets get back main directory
+      # lets get back main directory
+      popd > /dev/null
+  done
+
+  popd > /dev/null
+else 
+  for arg in "$@"
+  do
+    dir="$DOOR43_DIR/data/gitrepo/pages/$arg"
+echo $dir
+    if [ ! -e $dir ];
+    then
+      echo "No such language: $arg";
+      exit 1;
+    fi
+
+    pushd $dir > /dev/null
+    
+    git fetch --all
+    git reset --hard origin/master
+
     popd > /dev/null
-done
-
-popd > /dev/null
+  done
+fi
 
 echo "Done!"
 echo ""
