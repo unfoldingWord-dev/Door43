face68940912b91be247d34c4d2903ab42088370
Richard Mahn <richard_mahn@wycliffeassociates.org>
2015-10-07 22:34:40 -0400

    Made success text better for slackinvite

--- lib/plugins/slackinvite/lang/en/lang.php
+++ lib/plugins/slackinvite/lang/en/lang.php
@@ -11,6 +11,6 @@ $lang['btn_signup'] = 'Sign Up';
 $lang['name_err'] = 'Only letters and numbers are allowed in the name fields';
 $lang['email_err'] = 'Invalid email format';
 $lang['invite_success'] = '%1$s %2$s &lt;%3$s&gt; was sent an email with an invitation to join Slack.';
-$lang['invite_failed'] = '%1$s %2$s <%3$s> --> Invite failed.  Error:  %4$s';
+$lang['invite_failed'] = '%1$s %2$s &lt;%3$s&gt; --> Invite failed.  Error:  %4$s';
 $lang['success']='Invitation Sent';
 $lang['failed']='Invitation Failed';
