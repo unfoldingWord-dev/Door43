b8c87e101ba14e0770eac03ba90d9a2cb9c966d1
Richard Mahn <richard_mahn@wycliffeassociates.org>
2015-10-20 19:35:12 +0000

    updated door43 theme to contain new dokuwiki theme changes

--- lib/tpl/door43/main.php
+++ lib/tpl/door43/main.php
@@ -12,17 +12,17 @@ if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
 
 global $conf, $ACT, $lang, $ID;
 
-$hasSidebar = page_findnearest($conf['sidebar']);
-$showSidebar = $hasSidebar && ($ACT=='show');
+header('X-UA-Compatible: IE=edge,chrome=1');
 
 /** @var $translation helper_plugin_door43translation */
 $translation = &plugin_load('helper','door43translation');
 
+$hasSidebar = page_findnearest($conf['sidebar']);
+$showSidebar = $hasSidebar && ($ACT=='show');
 ?><!DOCTYPE html>
 <html lang="<?php echo $translation->getHtmlLang() ?>" dir="<?php echo $translation->getHtmlLangDir() ?>" class="no-js">
 <head>
     <meta charset="utf-8" />
-    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
     <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
     <script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
     <?php tpl_metaheaders() ?>
@@ -32,10 +32,9 @@ $translation = &plugin_load('helper','door43translation');
 </head>
 
 <body>
-    <!--[if lte IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
-    <div id="dokuwiki__site"><div id="dokuwiki__top"
-        class="dokuwiki site mode_<?php echo $ACT ?> <?php echo ($showSidebar) ? 'showSidebar' : '';
-        ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
+    <!--[if lte IE 8 ]><div id="IE8"><![endif]-->
+    <div id="dokuwiki__site"><div id="dokuwiki__top" class="site <?php echo tpl_classes(); ?> <?php
+        echo ($showSidebar) ? 'showSidebar' : ''; ?> <?php echo ($hasSidebar) ? 'hasSidebar' : ''; ?>">
 
         <?php include('tpl_header.php') ?>
 
@@ -43,24 +42,23 @@ $translation = &plugin_load('helper','door43translation');
 
             <?php if($showSidebar): ?>
                 <!-- ********** ASIDE ********** -->
-                <div id="dokuwiki__aside"><div class="pad include group">
+                <div id="dokuwiki__aside"><div class="pad aside include group">
                     <h3 class="toggle"><?php echo $lang['sidebar'] ?></h3>
-                    <div class="content">
+                    <div class="content"><div class="group">
                         <?php tpl_flush() ?>
-
 <?php
 if ($translation) echo $translation->showTranslations();
 ?>
-
                         <?php tpl_includeFile('sidebarheader.html') ?>
-                        <?php tpl_include_page($conf['sidebar'], 1, 1) ?>
+                        <?php tpl_include_page($conf['sidebar'], true, true) ?>
                         <?php tpl_includeFile('sidebarfooter.html') ?>
-                    </div>
+                    </div></div>
                 </div></div><!-- /aside -->
             <?php endif; ?>
 
             <!-- ********** CONTENT ********** -->
             <div id="dokuwiki__content"><div class="pad group">
+                <?php html_msgarea() ?>
 
                 <div class="pageId"><span><?php echo hsc($ID) ?></span></div>
 
@@ -74,12 +72,11 @@ if ($translation) echo $translation->showTranslations();
                 </div>
 
                 <div class="docInfo">
-
-<a href="<?php echo exportlink($ID)?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/txt.png" alt="Text Export"></a>&nbsp;
-<a href="<?php echo exportlink($ID, 'xhtmlbody')?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/htm.png" alt="HTML Export"></a>&nbsp;
-<a href="<?php echo exportlink($ID, 'odt')?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/odt.png" alt="ODT Export"></a><br />
-
-		<?php tpl_pageinfo() ?></div>
+  <a href="<?php echo exportlink($ID)?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/txt.png" alt="Text Export"></a>&nbsp;
+  <a href="<?php echo exportlink($ID, 'xhtmlbody')?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/htm.png" alt="HTML Export"></a>&nbsp;
+  <a href="<?php echo exportlink($ID, 'odt')?>"><img class="download" src="<?php echo DOKU_BASE?>lib/images/fileicons/odt.png" alt="ODT Export"></a><br />
+ 		  <?php tpl_pageinfo() ?>
+                </div>
 
                 <?php tpl_flush() ?>
             </div></div><!-- /content -->
@@ -95,12 +92,12 @@ if ($translation) echo $translation->showTranslations();
                             $data = array(
                                 'view'  => 'main',
                                 'items' => array(
-                                    'edit'      => tpl_action('edit',      1, 'li', 1, '<span>', '</span>'),
-                                    'revert'    => tpl_action('revert',    1, 'li', 1, '<span>', '</span>'),
-                                    'revisions' => tpl_action('revisions', 1, 'li', 1, '<span>', '</span>'),
-                                    'backlink'  => tpl_action('backlink',  1, 'li', 1, '<span>', '</span>'),
-                                    'subscribe' => tpl_action('subscribe', 1, 'li', 1, '<span>', '</span>'),
-                                    'top'       => tpl_action('top',       1, 'li', 1, '<span>', '</span>')
+                                    'edit'      => tpl_action('edit',      true, 'li', true, '<span>', '</span>'),
+                                    'revert'    => tpl_action('revert',    true, 'li', true, '<span>', '</span>'),
+                                    'revisions' => tpl_action('revisions', true, 'li', true, '<span>', '</span>'),
+                                    'backlink'  => tpl_action('backlink',  true, 'li', true, '<span>', '</span>'),
+                                    'subscribe' => tpl_action('subscribe', true, 'li', true, '<span>', '</span>'),
+                                    'top'       => tpl_action('top',       true, 'li', true, '<span>', '</span>')
                                 )
                             );
 
@@ -123,6 +120,6 @@ if ($translation) echo $translation->showTranslations();
 
     <div class="no"><?php tpl_indexerWebBug() /* provide DokuWiki housekeeping, required in all templates */ ?></div>
     <div id="screen__mode" class="no"></div><?php /* helper to detect CSS media query in script.js */ ?>
-    <!--[if ( lte IE 7 | IE 8 ) ]></div><![endif]-->
+    <!--[if lte IE 8 ]></div><![endif]-->
 </body>
 </html>
