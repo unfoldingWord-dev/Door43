f93ff3a0a4b800225786b08654e601a6d33f53dd
a-gundy <abigundy@gmail.com>
2015-11-24 10:41:52 -0500

    Added door43publishrequest plugin to add publish request form link to sitetools. Deleted line to remove media manager link from sitetools.

---
+++ lib/plugins/door43publishrequest/action.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 
+ * 
+ * @author  Abi Gundy <abigundy@gmail.com>
+ * @date 11-18-2015
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+class action_plugin_door43publishrequest extends DokuWiki_Action_Plugin {
+
+    /**
+     * Registers a callback function for a given event
+     *
+     * @param Doku_Event_Handler $controller DokuWiki's event controller object
+     * @return void
+     */
+    public function register(Doku_Event_Handler $controller) {
+
+       $controller->register_hook('TEMPLATE_SITETOOLS_DISPLAY', 'BEFORE' , $this, 'handle_template_sitetools_display');
+   
+    }
+
+    /**
+     * [Custom event handler which performs action]
+     *
+     * @param Doku_Event $event  event object by reference
+     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     *                           handler was registered]
+     * @return void
+     */
+
+    public function handle_template_sitetools_display(Doku_Event &$event, $param) {
+        
+        $url = 'http://td.unfoldingword.org/publishing/publish/request/';
+        $out = '<a href="'.$url.'"><li>'.$this->getLang('publishRequest').'</li></a>';
+        echo $out;
+
+    }
+
+}
+
